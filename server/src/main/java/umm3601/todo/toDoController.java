4c85cf6b73d6687fa8fdeea739f3311e08911086
carav008 <carav008@morris.umn.edu>
2017-02-27 17:36:09 -0600

    Work and Play with aggregates for Todo Summary
    Playing around with aggregates
    Issue #8

--- server/src/main/java/umm3601/todo/toDoController.java
+++ server/src/main/java/umm3601/todo/toDoController.java
@@ -14,12 +14,25 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
+import com.mongodb.Block;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Filters;
 
 import static com.mongodb.client.model.Filters.eq;
 
 public class toDoController {
 
     private final MongoCollection<Document> todoCollection;
+
+    Block<Document> printBlock = new Block<Document>() {
+        @Override
+        public void apply(final Document document) {
+            System.out.println(document.toJson());
+        }
+    };
+
 
     public toDoController() throws IOException {
 
@@ -29,6 +42,18 @@ public class toDoController {
         MongoDatabase db = mongoClient.getDatabase("test");
 
         todoCollection = db.getCollection("todos");
+
+// Trying to Figure out how aggregate works to get todo summary to work
+//        todoCollection.aggregate(
+//                Arrays.asList(
+//                        Aggregates.match(Filters.eq("status", "complete")),
+//
+//
+//                        Aggregates.group("owners", Accumulators.sum("count", 1))
+//                        Aggregates.group("category", Accumulators.sum("count", 1))
+//                )
+//        ).forEach(printBlock);
+
 
     }
 
@@ -40,21 +65,21 @@ public class toDoController {
             String owner = queryParams.get("owner")[0];
             filterDoc = filterDoc.append("owner", owner);
         }
-         //Filter category if defined
-        if(queryParams.containsKey("category")) {
-            String category = queryParams.get("category")[0];
-                    filterDoc = filterDoc.append("category", category);
-                }
+
          //Filter status if defined
         if(queryParams.containsKey("status")) {
             boolean status = Boolean.parseBoolean(queryParams.get("status")[0]);
             filterDoc = filterDoc.append("status", status);
         }
-
         //Filter contains if defined
         if (queryParams.containsKey("contains")) {
             String contains = queryParams.get("contains")[0];
             filterDoc = filterDoc.append("contains", contains);
+        }
+        //Filter category if defined
+        if(queryParams.containsKey("category")) {
+            String category = queryParams.get("category")[0];
+            filterDoc = filterDoc.append("category", category);
         }
 
 
