96100a06f7226451583d4c8c2ea64a952446183a
llopez005 <lopez477@morris.umn.edu>
2017-02-28 18:17:45 -0600

    Add methods to fiilter todos from server
    Added methods to populate from server
    Issue #2 #7 #6 #5

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -9,35 +9,71 @@ import { FilterBy } from "../users/filter.pipe";
     providers: [ FilterBy ]
 })
 
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
     public todos: Todo[];
-    public owners: Todo[];
-    public category: Todo[];
+    public filterOwner : string = "";
+    public filterCategory : string = "";
+    public filterBody : string = "";
+    public filterStatus : string = "";
+    public sortBy : string = "";
+    private freakingOut : boolean = false;
 
     constructor(private todoListService: TodoListService) {
         // this.todos = this.todoListService.getTodos();
     }
 
     ngOnInit(): void {
-        this.todoListService.getTodos().subscribe(
+        //     this.todoListService.getTodos().subscribe(
+        //         todos => this.todos = todos,
+        //         err => {
+        //             console.log(err);
+        //         }
+    };
+
+
+    private requestData(owner: string, category: string, status: string, body: string): string {
+        var parameter1 = new Array();
+        var searchAdd = "?";
+        var parameters = "";
+
+
+        if (owner != "")
+            parameter1["owner"] = owner;
+
+        if (category != "")
+            parameter1["category"] = category;
+
+        if (status != "")
+            parameter1["status"] = status;
+
+        if (body != "")
+            parameter1["body"] = body;
+
+
+        /**
+         * Construct params string by the value of the elements of which are enabled
+         */
+        for (let i = 0; i < parameter1.length; i++) {
+            parameters = parameters + searchAdd + i + "=" + parameter1[i]
+            searchAdd = "&"
+        }
+
+        // for (var p in parameter1) {
+        //     parameters += search + p + "=" + parameter1[p]; //Construct ?owner=Name&limit=3 etc.
+        //     search = "&";
+        // }
+        return parameters;
+    }
+
+    public request(owner: string, category: string, status: string, body: string) {
+        var req: string = this.requestData(owner, category, status, body);
+        this.todoListService.getTodos(owner, status, category).subscribe(
             todos => this.todos = todos,
             err => {
                 console.log(err);
             }
         );
 
-        //   this.todoListService.getTodoByOwner().subscribe(
-        //     owners => this.owners = owners,
-        //     err => {
-        //         console.log(err);
-        //     }
-        // );
-        //
-        // this.todoListService.getTodoByCategory().subscribe(
-        //     category => this.category = category,
-        //     err => {
-        //         console.log(err);
-        //     }
-        // );
+
     }
 }
--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -6,9 +6,24 @@ import { Observable } from "rxjs";
 @Injectable()
 export class TodoListService {
     private todoUrl: string = API_URL + "todos";
-    constructor(private http:Http) { }
 
-    getTodos(): Observable<Todo[]> {
+    constructor(private http: Http) {
+    }
+
+    getTodos(owner: string, status: string, category: string): Observable<Todo[]> {
+
+        if (owner) {
+            this.getTodoByOwner(owner);
+        }
+
+        if (status) {
+           this.getTodoByStatus(status);
+        }
+
+        if (category) {
+            this.getTodoByCategory(category);
+        }
+
         return this.http.request(this.todoUrl).map(res => res.json());
     }
 
@@ -16,22 +31,22 @@ export class TodoListService {
         return this.http.request(this.todoUrl + "/" + id).map(res => res.json());
     }
 
+
     getTodoByOwner(owner: string): Observable<Todo> {
-        return this.http.request(this.todoUrl+ "?owner=" + owner).map(res => res.json());
+        if (owner) {
+            return this.http.request(this.todoUrl + "?owner=" + owner).map(res => res.json());
+        }
     }
 
     getTodoByStatus(status: string): Observable<Todo> {
-        return this.http.request(this.todoUrl+ "?status=" + status).map(res => res.json());
+        if (status) {
+            return this.http.request(this.todoUrl + "?status=" + status).map(res => res.json());
+        }
     }
 
     getTodoByCategory(category: string): Observable<Todo> {
-        return this.http.request(this.todoUrl+ "?category=" + category).map(res => res.json());
+        if (category) {
+            return this.http.request(this.todoUrl + "?category=" + category).map(res => res.json());
+        }
     }
-
-    //Need this to work on the server side first
-    //  getTodoByContains(contains: string): Observable<Todo> {
-    //     return this.http.request(this.todoUrl+ "?=" + ).map(res => res.json());
-    // }
-
-
 }
